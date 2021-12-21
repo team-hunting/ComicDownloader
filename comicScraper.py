05da95790cdd30a978bc79511bb20a3b07b87ec6
Austin Hunting <austinwhunting@gmail.com>
2021-12-20 23:07:22 -0500

    added usage and som TODO's from my script

--- comicScraper.py
+++ comicScraper.py
@@ -1,18 +1,26 @@
-from bs4 import BeautifulSoup as bs 
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup as bs
 import os.path
 import requests
 import time
 import random
+import sys
 
-# To use this script:
-# Replace the startUrl variable with the url of the comic you want to scrape
-# Replace the comicTitle variable with the name of your comic
-# This script will create a folder in the current directory with the name of your comic
+# general TODO's:
+# TODO: give the user an option to download final images to a selected folder path
+# TODO: convert the images to CBZ files
+
+# geneneral TODO's pulled from aus's script:
+# TODO: add metadata from series page
+# TODO: add a check that the number of dowloaded images match with the number converted into the CBZ (needed?)
+# TODO: ensure all image types are grabbed (jpg, png, gif, etc.) from the blogspot links (needed?)
+# TODO: add a function to just download images, and one to just convert named images to CBZ's
+# TODO: add an option to just pass the search term and download all the series in the results (hit the search API)
+# TODO: add and option for downloading high or low quality versions of the images
 
 prefix = "https://readcomiconline.li"
 readType = "&readType=1" # Suffix for issue URLs that makes it show all images on one page
-startURL = "https://readcomiconline.li/Comic/Lucifer-2016" # The comic you want to download
-comicTitle = "Lucifer" # Determines the folder to save the images in
 script_dir = os.path.dirname(__file__)
 counter = 0 # Image Numbers
 path = script_dir + "\\" + comicTitle + "\\" # This is a windows path, replace forward slashes with backslashes for Linux
@@ -52,7 +60,7 @@ def scrapeImageLinksFromIssue(url):
     soup = bs(req.content, 'html.parser')
     soup = soup.prettify()
     lines = soup.split("\n")
-    
+
     imageLinks = []
 
     for line in lines:
@@ -66,7 +74,7 @@ def extractImageUrlFromText(text):
     urlEnd = text.find("s1600")
     urlStart = text.find("https")
     return text[urlStart:urlEnd+5]
-    
+
 def saveImagesFromImageLinks(imageLinks):
     for imageLink in imageLinks:
         saveImageFromUrl(imageLink)
@@ -92,7 +100,7 @@ def main():
     for issue in issues:
         issueLink = prefix + issue + readType
         issueLinks.append(issueLink)
-    
+
     print("Issue Links:")
     print(issueLinks)
     print()
@@ -102,7 +110,7 @@ def main():
         issueImageLinks = scrapeImageLinksFromIssue(issueLink)
         imageLinks.append(issueImageLinks)
         time.sleep(random.randint(10,20))
-        
+
     print("Image Links:")
     for issue in imageLinks: #all issues in their own sub-arrays
         print(issue)
@@ -111,8 +119,31 @@ def main():
     for issue in imageLinks:
         saveImagesFromImageLinks(issue)
 
+def usage():
+    print("Script for downloading CBZ files from readcomiconline.li")
+    print("")
+    print("Usage: comicScraper.py <url> <comicTitle>")
+    print("")
+    print("Arguments:")
+    print("  - url: The url of the comic you want to download")
+    print("  - comicTitle: The folder location to save the CBZ file to")
+    print("                (same as path of script execution)")
+    print("")
+    print("Example: comicScraper.py https://readcomiconline.li/Comic/Lucifer-2016 Lucifer")
 
-
+# TODO: if the user doesn't supply a folder name strip it from the url
 if __name__ == "__main__":
+    # TODO: use argparse here to get filename and url over sys
+    # TODO: should look like <script-name> -u / -url <url> -f / -folder <folder>
+    if len(sys.argv) <= 2:
+        usage()
+        sys.exit()
+
+    startURL = str(sys.argv[1])
+    comicTitle = str(sys.argv[2])
+
+    print(f"Starting to scrape {comicTitle} from {startURL}")
+
     main()
+    # TODO: add a check that the file got downloaded and converted to a CBZ
     print("\n Comic Downloaded")
